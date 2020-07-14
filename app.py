41524561f597431b6d8789288ec1686ce58ab432
AliKarami <karamiali19@gmail.com>
2020-07-14 23:41:53 +0100

    Added basic validation functionality

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from data_fetcher import fetch
+from helpers import fetch
 
 baseUri = 'https://catalogue-labs.tax.service.gov.uk/service-configs/config-by-key/'
 serviceName = 'business-tax-account'
+blacklist = ['localhost']
+configWhiteList = []
 
 response = fetch(baseUri + serviceName)
 
 for key in response.keys():
-  print(response.get(key).get('qa'))
-  print(response.get(key).get('staging'))
-  print(response.get(key).get('production'))
-
+  environment = response.get(key).get('production')
+  if environment is not None:
+    environment = sorted(environment, key=lambda i: i['precedence'], reverse=True)[0]
+    environmentValue = environment.get('value').lower()
+    for item in blacklist:
+      if item in environmentValue and key not in configWhiteList:
+        print(f"key:\t{key}")
+        print(f"source:\t{environment.get('source')}")
+        print(f"value:\t{environmentValue}")
+        print('---------------------------')
